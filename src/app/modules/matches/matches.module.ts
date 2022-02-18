4eb53fbdfe3f47a68c086098bb3d545320a3dd70
jesusglez96 <jezu96@gmail.com>
2022-02-18 09:36:21 +0100

    first commit blank

---
+++ src/app/modules/matches/matches.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatchComponent } from './match/match.component';
+
+
+
+@NgModule({
+  declarations: [
+    MatchComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MatchesModule { }
